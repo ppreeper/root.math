3f6687e6ea12b11d46d1d65d94a7da63cb026cc8
Peter Preeper <ppreeper@gmail.com>
2020-01-14 15:09:55 -0700

    add more tests and special cases

--- root.go
+++ root.go
@@ -4,7 +4,17 @@ import (
 	"math"
 )
 
-func root(x, p float64) float64 {
+func Root(x, p float64) float64 {
+	switch {
+	case p == 2:
+		return math.Sqrt(x)
+	case p == 3:
+		return math.Cbrt(x)
+	case x == 0 || IsNaN(x) || IsInf(x, 1):
+		return x
+	case x < 0 && math.Mod(x, 2) == 0:
+		return x
+	}
 	y := x / p
 	for i := 0; i < 32; i++ {
 		y = (x/math.Pow(y, (p-1)) + y*(p-1)) / p
--- root_test.go
+++ root_test.go
@@ -1,27 +1,49 @@
 package root
 
-import "testing"
+import (
+	"math"
+	"testing"
+)
 
 var mulRootTests = []struct {
 	x        float64
 	p        float64
 	expected float64
 }{
-	{5, 2, 2.23606797749979},
-	{29, 3, 3.072316825685847},
-	{27, 3, 3.0},
+	{5, 2, math.Sqrt(5)},
+	{29, 3, math.Cbrt(29)},
+	{27, 3, math.Cbrt(27)},
 }
 
 func TestRoot(t *testing.T) {
 	for _, mt := range mulRootTests {
-		r := root(mt.x, mt.p)
+		r := Root(mt.x, mt.p)
 		if r != mt.expected {
 			t.Errorf("\nexpected %f got %f", mt.expected, r)
 		}
 	}
 }
-func BenchmarkRoot(b *testing.B) {
+
+// Square Root
+func BenchmarkSqrt(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		root(29, 3)
+		math.Sqrt(29)
+	}
+}
+func BenchmarkRoot2(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Root(29, 2)
+	}
+}
+
+// Cube Root
+func BenchmarkCbrt(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		math.Cbrt(29)
+	}
+}
+func BenchmarkRoot3(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		Root(29, 3)
 	}
 }
